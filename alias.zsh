b4619fa899a4dc0bf3c4d7283e6cce8aa0b43af8
weikai <weikaifu@microsoft.com>
2025-10-11 10:32:25 +0000

    add zsh alias

---
+++ alias.zsh
@@ -0,0 +1,24 @@
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ~="cd ~"
+alias c="clear"
+alias /="cd /"
+
+alias md="mkdir -p"
+alias rd="rmdir"
+
+alias l="eza -lh"
+alias la="eza -lha"
+alias ll="eza -lh"
+
+alias gsts="git status"
+alias gco="git checkout"
+alias gcm="git commit -m"
+alias gl="git pull"
+alias gp="git push"
+alias ga="git add"
+alias gb="git branch"
+alias gd="git diff"
+
+alias lg="lazygit"
