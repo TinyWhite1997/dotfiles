fd7005714cff6bd3f63717fc4b48220b118e4599
TinyWhite1997 <fwk675725@163.com>
2025-10-15 21:42:34 +0800

    setup roslyn

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -76,6 +76,7 @@ vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-vim.lsp.enable "roslyn_lsp"
+
+vim.lsp.enable "roslyn_ls"
 
 require "polish"
