fda3a600ca4adc93de5c3f72a1dfad107a83fa69
TinyWhite1997 <fwk675725@163.com>
2025-10-15 09:53:23 +0800

    try roslyn_lsp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -76,5 +76,6 @@ vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.lsp.enable "roslyn_lsp"
 
 require "polish"
---
+++ config/nvim/lsp/roslyn_lsp.lua
@@ -0,0 +1,11 @@
+return {
+  cmd = {
+    "dotnet",
+    "/Users/fuweikai/.roslyn/lib/net9.0/Microsoft.CodeAnalysis.LanguageServer.dll",
+    "--logLevel",
+    "Information",
+    "--extensionLogDirectory",
+    "/Users/fuweikai/.roslyn/logs",
+    "--stdio",
+  },
+}
