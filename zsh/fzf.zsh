ace1e5fa48940b6a0e3c8d2ced8a2ada6790c595
TinyWhite1997 <fwk675725@163.com>
2025-10-21 09:16:48 +0800

    zsh config git and fzf

---
+++ zsh/fzf.zsh
@@ -0,0 +1,37 @@
+export FZF_DEFAULT_OPTS="
+  --tmux 80%
+  --layout=reverse 
+  --border
+  --color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934"
+  
+export FZF_CTRL_T_COMMAND='fd --type f --hidden --follow --exclude .git,node_modules'
+export FZF_CTRL_T_OPTS="
+  --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1
+  --preview 'bat --color=always --style=numbers --line-range :500 {}'"
+  
+export FZF_CTRL_R_OPTS="
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --bind '?:toggle-preview'
+  --preview 'echo {}' 
+  --preview-window down:3:hidden:wrap
+  --color=header:italic,bg+:#3F3F3F,bg:#4B4B4B,border:#6B6B6B,spinner:#98BC99,hl:#719872,fg:#D9D9D9,header:#719872,info:#BDBB72,pointer:#E12672,marker:#E17899,fg+:#D9D9D9,preview-bg:#3F3F3F,prompt:#98BEDE,hl+:#98BC99
+  --header 'Ctrl-Y: Copy command to clipboard; ALT-R: Raw mode; ?: Toggle preview'"
+  
+export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git,node_modules'
+export FZF_ALT_C_OPTS="
+  --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1
+  --preview 'tree -C {} | head -200"
+
+# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+export FZF_COMPLETION_TRIGGER='jj'
