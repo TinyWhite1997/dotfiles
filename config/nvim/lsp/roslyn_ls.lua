b89b0fcb631f2d7c66f5fe2b4722ac76ecc980ac
weikai <weikaifu@microsoft.com>
2025-10-21 18:28:29 +0800

    add windows roslyn lsp path

--- config/nvim/lsp/roslyn_ls.lua
+++ config/nvim/lsp/roslyn_ls.lua
@@ -98,11 +98,10 @@ local dll_path = ""
 if sysname == "Darwin" then
   dll_path = "/Users/fuweikai/.roslyn/content/LanguageServer/osx-arm64/Microsoft.CodeAnalysis.LanguageServer.dll"
 elseif sysname == "Linux" then
-  -- TODO: add Linux dll path
   dll_path = "/home/weikai/.dotnet_lsp/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer.dll"
 elseif sysname == "Windows_NT" then
-  -- TODO: add Windows dll path
-  dll_path = ""
+  dll_path =
+    "C:\\Users\\weikaifu\\.dotnet_lsp\\content\\LanguageServer\\win-x64\\Microsoft.CodeAnalysis.LanguageServer.dll"
 end
 
 ---@type vim.lsp.Config
