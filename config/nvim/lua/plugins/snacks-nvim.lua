c271ac131b8643d47c0a747334baa1986e9caeb3
weikai <weikaifu@microsoft.com>
2025-10-22 16:51:29 +0800

    fix hover

--- config/nvim/lua/plugins/snacks-nvim.lua
+++ config/nvim/lua/plugins/snacks-nvim.lua
@@ -57,6 +57,8 @@ return {
     { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "gh", vim.lsp.buf.hover, desc = "Hover" },
+    { "<leader>lR", vim.lsp.buf.rename, desc = "LSP Rename" },
     -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     -- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
     -- Other
