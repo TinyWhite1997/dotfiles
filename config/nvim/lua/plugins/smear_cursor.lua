475c3bc4941795f2180f8929900f954157bf07e5
TinyWhite1997 <fwk675725@163.com>
2025-12-18 20:34:52 +0800

    remove smear_cursor plugin

--- config/nvim/lua/plugins/smear_cursor.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.neovide then return {} end
-if vim.env.KITTY_WINDOW_ID then return {} end
-if vim.env.GHOSTTY_RESOURCES_DIR then return {} end
-return {
-  "sphamba/smear-cursor.nvim",
-  opts = {},
-}
