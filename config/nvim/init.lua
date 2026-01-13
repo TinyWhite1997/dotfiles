5bd2de5e046c5c71cdbd1d8e0b5a9e08680b6083
weikai <weikaifu@microsoft.com>
2026-01-13 11:22:08 +0800

    updating

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -82,7 +82,7 @@ if vim.g.neovide then
   vim.g.neovide_light_radius = 5
   if sysname == "Darwin" then vim.g.neovide_opacity = 0.9 end
 
-  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_hide_mouse_when_typing = false
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
   vim.g.neovide_input_ime = false
 
