06814d42155b2a01d8114743607b16df092618de
Weikai Fu <weikaifu+microsoft@microsoft.com>
2026-09-08 11:03:19 +0800

    Install Windows CLI tools with winget

--- install.ps1
+++ install.ps1
@@ -8,6 +8,25 @@ $BASEDIR = $PSScriptRoot
 
 Set-Location $BASEDIR
 Get-Location
+
+foreach ($PACKAGE in @(
+    'Gyan.FFmpeg'
+    '7zip.7zip'
+    'jqlang.jq'
+    'oschwartz10612.Poppler'
+    'sharkdp.fd'
+    'BurntSushi.ripgrep.MSVC'
+    'junegunn.fzf'
+    'ajeetdsouza.zoxide'
+    'ImageMagick.ImageMagick'
+    'sxyazi.yazi'
+)) {
+    if (winget list --id $PACKAGE --exact --source winget --accept-source-agreements | Select-String -Pattern $PACKAGE -SimpleMatch -Quiet) {
+        continue
+    }
+    winget install --id $PACKAGE --exact --source winget --silent --accept-package-agreements --accept-source-agreements --disable-interactivity
+}
+
 Set-Location $DOTBOT_DIR
 git submodule update --init --recursive
 foreach ($PYTHON in ('python', 'python3')) {
